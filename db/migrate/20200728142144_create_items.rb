0b64158270b2e73015a6e9abe843752a132e0db9
covalt-ta <taku.covayashi@gmail.com>
2020-07-28 23:35:49 +0900

    マイグレーションファイルの編集

--- db/migrate/20200728142144_create_items.rb
+++ db/migrate/20200728142144_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.text :text, null: false
+      t.integer :price, null: false
+      t.integer :item_category_id, null: false
+      t.integer :shipping_status_id, null: false
+      t.integer :shipment_prefecture_id, null: false
+      t.integer :shipping_fee_id, null: false
+      t.integer :shipping_day_id, null: false
       t.timestamps
     end
   end

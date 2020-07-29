ceb6a91661f83078d8590f9f0221481b9ca6623b
covalt-ta <taku.covayashi@gmail.com>
2020-07-30 08:05:01 +0900

    priceカラムにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates :name, :text, :price, :item_category, :item_status,
             :shipment_prefecture, :shipping_day, :shipping_fee, presence: true
-  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   with_options numericality: { other_than: 1 } do
     validates :item_category_id, :item_status_id, :shipment_prefecture_id,
               :shipping_day_id, :shipping_fee_id

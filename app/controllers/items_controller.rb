897bf55681bd770bd38fbd7ea1a49e20fb7b311e
covalt-ta <taku.covayashi@gmail.com>
2020-07-29 17:16:51 +0900

    画像投稿の為のビューファイル修正
    ・ルーティングの追加
    ・new.html.erbの編集
    ・コントローラーにnew・createアクションを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,15 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    # params.requite().permit()
+  end
 end

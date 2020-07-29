8708b9fade307e9cdb49793d0eb08f1fe6323798
covalt-ta <taku.covayashi@gmail.com>
2020-07-29 14:25:43 +0900

    itemモデルにバリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_category
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :shipment_prefecture
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :shipping_fee
+
+  validates :name, :text, :price, :item_category, :item_status,
+            :shipment_prefecure, :shpping_day, :shipping_fee, presence: true
+  
+  with_options numericality: { other_than: 1 } do
+    validates :item_category_id, :item_status_id, :shipment_prefecture_id,
+              :shipping_day_id, :shipping_fee_id
+  end
 end
--- db/migrate/20200728142144_create_items.rb
+++ db/migrate/20200728142144_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :text, null: false
       t.integer :price, null: false
       t.integer :item_category_id, null: false
-      t.integer :shipping_status_id, null: false
+      t.integer :item_status_id, null: false
       t.integer :shipment_prefecture_id, null: false
       t.integer :shipping_fee_id, null: false
       t.integer :shipping_day_id, null: false

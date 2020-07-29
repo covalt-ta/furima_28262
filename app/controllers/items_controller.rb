dc8cc753cddd27f7e28fc06a94e111d7d1a2ce93
covalt-ta <taku.covayashi@gmail.com>
2020-07-29 18:28:21 +0900

    商品投稿画面で投稿して保存できる
    一覧画面での画像表示も問題なし

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
   end
 
   private
   def item_params
-    # params.requite().permit()
+    params.require(:item).permit(:name, :text, :price, :imege, :item_category_id, :item_status_id, :shipping_fee_id, :shipment_prefecture_id, :shipping_day_id)
   end
 end

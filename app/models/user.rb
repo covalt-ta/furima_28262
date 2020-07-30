948f5ceac67d79e758f9f2eb721b9826bf41531c
covalt-ta <taku.covayashi@gmail.com>
2020-07-30 19:25:09 +0900

    Userモデルにバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-
+  validates :nickname, :last_name, :first_name,
+            :last_name_kana, :first_name_kana, :birthday, presence: true
 end

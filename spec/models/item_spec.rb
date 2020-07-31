b86fcb7ce0cbd1f8595b18549885617b377f41d0
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 15:40:24 +0900

    Itemモデルの単体テストの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/test_image.jpeg')
+  end
+  describe '商品の保存' do
+    context '商品の保存ができる場合'do
+      it 'ユーザーが紐付いていて全ての項目が必要条件を満たしていると保存される' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品の保存ができない場合' do
+      it 'ユーザーが紐付いていないと保存ができない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it 'nameが空だと保存できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'nameが40文字より多いと保存できない' do
+        @item.name = "0" * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
+      it 'textが空だと保存できない' do
+        @item.text = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'textが1,000文字より多いと保存できない' do
+        @item.text = "0" * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text is too long (maximum is 1000 characters)")
+      end
+      it '価格が空だと保存できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300円未満だと保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9,999,999円より大きいと保存できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'カテゴリが選択されていないと保存できない' do
+        @item.item_category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
+      it '商品の状態が選択されていないと保存できない' do
+        @item.item_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
+      end
+      it '配送料の負担が選択されていないと保存できない' do
+        @item.shipping_fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      it '発送元の地域が選択されていないと保存できない' do
+        @item.shipment_prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipment prefecture can't be blank")
+      end
+      it '発送までの日数が選択されていないと保存できない' do
+        @item.shipping_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+    end
+  end
 end

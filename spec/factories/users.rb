86647211bacdf9b642cfb30c96b469cf1f17e26e
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 14:42:12 +0900

    Itemモデル単体テストのexampleの整理

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname               {Faker::Name.initials(number: 4)}
     email                  {Faker::Internet.free_email}
-    password               {Faker::Internet.password(mini_length: 6)}
-    password_cofirmation   {password}
+    password               {Faker::Internet.password(min_length: 6)}
+    password_confirmation  {password}
     last_name              {Gimei.last.kanji}
     first_name             {Gimei.first.kanji}
     last_name_kana         {Gimei.last.katakana}

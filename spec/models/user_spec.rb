cb8b893a1bc11e6da8f13b72b19a2df669dedcfa
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 21:50:03 +0900

    Userモデルの単体テストのexampleの洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = Factorybot.build(:user)
+    end
+    context 'ユーザー作成ができる場合' do
+      it '全ての項目が正しく存在すれば登録できる' do
+      
+      end
+      it 'passwordが6文字以上で登録できる' do
+      
+      end
+    end
+    context 'ユーザー作成ができない場合' do
+      it 'nicknameが空では登録できない' do
+        
+      end
+      it 'emailが空では登録できない' do
+        
+      end
+      it 'emailに＠が含まれていないと登録できない' do
+        
+      end
+      it 'emailが他のユーザーと重複していると登録できない' do
+        
+      end
+      it 'passwordが6文字より少ないと登録できない' do
+        
+      end
+      it 'passwordとpassword_confirmationが一致しないと登録できない' do
+      
+      end  
+      it 'last_nameが空だと登録できない' do
+        
+      end
+      it 'last_nameが全角（漢字・ひらがな・カタカナ）でければ登録できない' do
+        
+      end
+      it 'first_nameが空だと登録できない' do
+        
+      end
+      it 'first_nameが全角（漢字・ひらがな・カタカナ）でければ登録できない' do
+        
+      end
+      it 'last_name_kanaが空だと登録できない' do
+        
+      end
+      it 'last_name_kanaが全角（カタカナ）でければ登録できない' do
+        
+      end
+      it 'first_name_kanaが空だと登録できない' do
+        
+      end
+      it 'first_name_kanaが全角（カタカナ）でければ登録できない' do
+        
+      end
+      it 'birthdayの「年」が空だと登録できない' do
+        
+      end
+      it 'birthdayの「月」が空だと登録できない' do
+        
+      end
+      it 'birthdayの「日」が空だと登録できない' do
+        
+      end
+    end
+  end
 end

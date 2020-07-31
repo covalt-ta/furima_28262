30935a0630fb945b0aa48c25d4110770353ffdfa
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 13:07:26 +0900

    factory_bot,Fakerの準備、設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname               {Faker::Name.initials(number: 4)}
+    email                  {Faker::Internet.free_email}
+    password               {Faker::Internet.password(mini_length: 6)}
+    password_cofirmation   {password}
+    last_name              {Gimei.last.kanji}
+    first_name             {Gimei.first.kanji}
+    last_name_kana         {Gimei.last.katakana}
+    first_name_kana        {Gimei.first.katakana}
+    birthday               {Faker::Date.birthday(min_age: 12, max_age: 80)}
   end
 end

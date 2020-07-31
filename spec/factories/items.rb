dd2fc2271ac75a13c1d2c1d80009bdd73a15f1e8
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 19:00:46 +0900

    itemのFactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                   { Faker::Book.title }
     text                   { Faker::Lorem.sentence }
-    price                  { Faker::Number.between(from: 300, to: 9_999_999) }
-    item_category_id       { Faker::Number.between(from: 2, to: 11) }
-    item_status_id         { Faker::Number.between(from: 2, to: 7) }
-    shipping_fee_id        { Faker::Number.between(from: 2, to: 3) }
-    shipment_prefecture_id { Faker::Number.between(from: 2, to: 48) }
-    shipping_day_id        { Faker::Number.between(from: 2, to: 4) }
+    price                  { Faker::Number.between(from: 300, to: 9999999) }
+    item_category_id       { 2 }  
+    item_status_id         { 2 }
+    shipping_fee_id        { 2 }
+    shipment_prefecture_id { 2 }
+    shipping_day_id        { 2 }
     association :user
   end
 end

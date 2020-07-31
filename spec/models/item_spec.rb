0d35fff0cae498159af0db452d76527903e1c588
covalt-ta <taku.covayashi@gmail.com>
2020-07-31 15:45:54 +0900

    Itemモデル単体テストの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
+      it '画像が空だと保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it 'nameが空だと保存できない' do
         @item.name = nil
         @item.valid?

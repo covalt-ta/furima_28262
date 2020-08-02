db612ec92df5007e95e919fe23b21fa02711c1d9
covalt-ta <taku.covayashi@gmail.com>
2020-08-02 16:31:07 +0900

    ordersコントローラーformオブジェクトcreateアクション作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,24 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(
+      :postal_code, :shipment_prefecture_id,
+      :city, :block, :building, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end

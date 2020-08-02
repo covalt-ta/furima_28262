843aba5948785cd166aa0003912b06040e6e5115
covalt-ta <taku.covayashi@gmail.com>
2020-08-02 16:29:36 +0900

    フォームオブジェクトの修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,11 +1,12 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipment_prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :postal_code, :shipment_prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id
 
   #バリデーションは基本動作確認後に実装
 
   def save
-    address = Address.create(
+    Order.create(item_id: item_id, user_id: user_id)
+    Address.create(
       postal_code: postal_code,
       shipment_prefecture_id: shipment_prefecture_id,
       city: city,
@@ -14,5 +15,5 @@ class OrderAddress
       phone_number: phone_number,
       item_id: item_id
     )
-    Order.create(user_id: user_id, item_id: address.item_id)
+  end
 end

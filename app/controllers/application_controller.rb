7ebc12ae3c6265b235366d99e4ccaacb92038875
covalt-ta <taku.covayashi@gmail.com>
2020-08-03 21:51:00 +0900

    ログイン前のページに遷移させる設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
+  def store_current_location
+    store_location_for(:user, request.fullpath)
   end
 end

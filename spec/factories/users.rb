86bc620694cdc7343ce686cc62bda24e9f0b2371
covalt-ta <taku.covayashi@gmail.com>
2020-08-04 16:40:04 +0900

    UserのFactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname               { Faker::Name.initials(number: 4) }
     email                  { Faker::Internet.free_email }
-    password               { Faker::Internet.password(min_length: 6) }
+    password               { Faker::Lorem.characters(number: 6, min_alpha: 4, min_numeric: 2) }
     password_confirmation  { password }
     last_name              { Gimei.last.kanji }
     first_name             { Gimei.first.kanji }

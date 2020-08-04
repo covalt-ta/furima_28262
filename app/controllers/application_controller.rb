f973775cc16c90f3b51bedd8f4eaa2e07ec9ff3b
covalt-ta <taku.covayashi@gmail.com>
2020-08-04 22:11:39 +0900

    basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_current_location, unless: :devise_controller?
 
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def store_current_location
     store_location_for(:user, request.fullpath)
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do | username, password |
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
